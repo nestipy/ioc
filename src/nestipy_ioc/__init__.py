e03ba639821d4fa0d5dc437cc4cda2170e202d43
tsiresymila <tsiresymila@gmail.com>
2024-04-24 09:55:03 +0300

    Add middleware container

--- src/nestipy_ioc/__init__.py
+++ src/nestipy_ioc/__init__.py
@@ -3,6 +3,7 @@ from .container import NestipyContainer
 from .context_container import NestipyContextContainer
 from .dependency import Inject, Res, Req, Session, Query, Body, Args, Context, Files, SocketServer, SocketClient, \
     SocketData, Params
+from .middleware import MiddlewareProxy, MiddlewareRouteConfig, MiddlewareContainer
 from .provider import ModuleProviderDict
 
 __all__ = [
@@ -22,5 +23,8 @@ __all__ = [
     "SocketServer",
     "SocketClient",
     "SocketData",
-    "Params"
+    "Params",
+    "MiddlewareProxy",
+    "MiddlewareRouteConfig",
+    "MiddlewareContainer"
 ]
